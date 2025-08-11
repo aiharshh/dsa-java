8a3c0a3c5a5f87e4b30ca0cfe92c57d84ae0d47b
aiharshh <aiharshhublikar@gmail.com>
2025-08-11 11:02:47 +0530

    Array traversal

--- src/recursion/ArrayTraversal.java
+++ src/recursion/ArrayTraversal.java
@@ -5,8 +5,15 @@ public class ArrayTraversal {
 		System.out.print(arr[i]+" ");
 		print(i+1,arr);
 	}
+	// This is for printing continuous numbers
+//	public static void print2(int i,int n) {
+//		if(i>n) return;
+//		System.out.print(i+" ");
+//		print(i+1,n);
+//	}
 	public static void main(String[] args) {
 		int[] arr = {4,5,5,2,2,2,1};
 		print(0,arr);
+//		print2(1, 10);
 	}
 }
