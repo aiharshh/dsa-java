68fb80d0eed35e7ec0f6f5ed786b4e6036a75a2a
aiharshh <aiharshhublikar@gmail.com>
2025-08-12 10:47:16 +0530

    SkipACharacter Code updates

--- src/recursion/SkipACharacter.java
+++ src/recursion/SkipACharacter.java
@@ -1,18 +1,16 @@
 package recursion;
 
 public class SkipACharacter {
-	public static void skip (int i, String s, String ans) {
-		if(i==s.length()) {
-			System.out.println(ans);
+	public static void skip(int i, String s, String ans){
+		if(i==s.length()){
+			System.out.print(ans);
 			return;
 		}
 		if(s.charAt(i)!='a') ans += s.charAt(i);
-		skip(i+1,s,ans);
+		skip(i+1, s, ans);
 	}
 	public static void main(String[] args) {
-		String s = "Raghav Garg";
-//		String ans = "";
-//		skip(0,s,ans);
-		skip(0,s,"");
+		String s = "Harsh Hublikar";
+		skip(0, s, "");
 	}
 }
