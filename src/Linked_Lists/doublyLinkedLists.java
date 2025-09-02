5549c67b0da9ee75b8dd8465f586ea4a894d1276
aiharshh <aiharshhublikar@gmail.com>
2025-09-02 10:56:48 +0530

    DLL's insertAtHead, insertAtTail updated

--- src/Linked_Lists/doublyLinkedLists.java
+++ src/Linked_Lists/doublyLinkedLists.java
@@ -10,7 +10,58 @@ class dNode{
 }
 // user defined data structure
 class DLL{
+    public dNode head;
+    public dNode tail;
+    int size = 0;
 
+    void display(){
+        dNode temp = head;
+        while(temp!=null){
+            System.out.print(temp.val + " ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+    void insertAtTail(int val){
+        dNode temp = new dNode(val);
+        if(head==null && tail==null) head = tail = temp;
+        else{
+            tail.next = temp;
+            temp.prev = tail;
+            tail = temp;
+            tail.next = null;
+        }
+        size++;
+    }
+    void insertAtHead(int val){
+        dNode temp = new dNode(val);
+        if(head==null && tail==null) head = tail = temp;
+        else{
+            temp.next = head;
+            head.prev = temp;
+            head = temp;
+            head.prev = null;
+        }
+        size++;
+    }
+    void insert(int idx, int val){
+        dNode temp = new dNode(val);
+        if(head==null && tail==null) head = tail = temp;
+        else{
+            dNode x = head;
+            dNode y = head;
+            for(int i=0;i<idx-1;i++){
+                x = x.next;
+            }
+            for(int i=0;i<=idx-1;i++){
+                y = y.next;
+            }
+            x.next = temp;
+            temp.prev = x;
+            y.prev = temp;
+            temp.next = y;
+        }
+    }
 }
 public class doublyLinkedLists {
     public static void print(dNode head){
@@ -39,6 +90,20 @@ public class doublyLinkedLists {
     }
     public static void main(String[] args) {
         DLL list = new DLL();
+        list.insertAtTail(10);
+        list.insertAtTail(20);
+        list.insertAtTail(30);
+        list.display();
+        list.insertAtTail(40);
+        list.display();
+        System.out.println(list.size);
+        list.insertAtHead(50);
+        list.display();
+        System.out.println(list.size);
+        list.insert(2, 60);
+        list.display();
+        list.insert(2, 70);
+        list.display();
 //        dNode a = new dNode(10);
 //        dNode b = new dNode(20);
 //        dNode c = new dNode(30);
