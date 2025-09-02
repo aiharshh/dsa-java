b35b13ff93c8d5554cb0fbbefbb4a5218e4cef41
aiharshh <aiharshhublikar@gmail.com>
2025-09-02 17:23:41 +0530

    Hello

--- src/Linked_Lists/doublyLinkedLists.java
+++ src/Linked_Lists/doublyLinkedLists.java
@@ -25,7 +25,6 @@ class DLL {
         }
         System.out.println();
     }
-
     void insertAtTail(int val) {
         dNode temp = new dNode(val);
         if (head == null && tail == null) head = tail = temp;
@@ -37,7 +36,6 @@ class DLL {
         }
         size++;
     }
-
     void insertAtHead(int val) {
         dNode temp = new dNode(val);
         if (head == null && tail == null) head = tail = temp;
@@ -45,11 +43,9 @@ class DLL {
             temp.next = head;
             head.prev = temp;
             head = temp;
-            head.prev = null;
         }
         size++;
     }
-
     void insert(int idx, int val) {
         dNode temp = new dNode(val);
         if (idx < 0 || idx > size) {
@@ -75,6 +71,90 @@ class DLL {
         y.prev = temp;
         temp.next = y;
         size++;
+    }
+
+    void deleteAtHead(){
+        if(head==null){
+            System.out.println("Can't delete when Linked Lists is Empty");
+            return;
+        }
+        if(head==tail){
+            head = tail = null;
+            size--;
+            return;
+        }
+        head = head.next;
+        head.prev = null;
+        size--;
+    }
+    void deleteAtTail(){
+        if(tail==null){
+            System.out.println("Can't delete when Linked List is Empty");
+            return;
+        }
+        if(head==tail){
+            head = tail = null;
+            size--;
+            return;
+        }
+        tail = tail.prev;
+        tail.next = null;
+        size--;
+    }
+    void delete(int idx){
+        if(idx>=size || idx<0){
+            System.out.println("Index doesn't exist in Linked Lists");
+            return;
+        }
+        if(idx==0){
+            deleteAtHead();
+            return;
+        }
+        if(idx==size-1){
+            deleteAtTail();
+            return;
+        }
+        dNode a = head;
+        for(int i=0;i<idx-1;i++){
+            a = a.next;
+        }
+        dNode b = a.next.next;
+        a.next = b;
+        b.prev = a;
+        size--;
+    }
+
+    void get(int idx){
+        dNode t = head;
+        if(idx<0 || idx>=size){
+            System.out.println("Invalid Index");
+            return;
+        }
+        if(idx==0){
+            System.out.println("The value at head "+ head.val);
+            return;
+        }
+        if(idx==size-1){
+            System.out.println("The value at tail "+ tail.val);
+            return;
+        }
+        for(int i=0;i<idx;i++){
+            t = t.next;
+        }
+        System.out.println("The value at Index "+idx+" is "+t.val);
+    }
+    void set(int idx, int val){
+        if(idx<0 || idx>=size){
+            System.out.println("Invalid Index");
+            return;
+        }
+        if(idx==0){ head.val = val; return; }
+        if(idx==size-1){ tail.val = val; return; }
+        dNode temp = head;
+        for(int i=0;i<idx;i++){
+            temp = temp.next;
+        }
+        temp.val = val;
     }
 }
 
@@ -125,6 +205,20 @@ public class doublyLinkedLists {
         list.insert(0, 90);
         list.display();
         list.insert(8, 99);
+        list.display();
+        list.deleteAtHead();
+        list.display();
+        list.deleteAtTail();
+        list.display();
+        list.deleteAtTail();
+        list.display();
+        list.delete(2);
+        list.display();
+        System.out.println(list.size);
+        list.get(2);
+        list.get(0);
+        list.get(4);
+        list.set(2, 100);
         list.display();
 //        dNode a = new dNode(10);
 //        dNode b = new dNode(20);
