a0c17e04e5f435279393238ba8c26b7bf0c172c1
aiharshh <aiharshhublikar@gmail.com>
2025-10-05 15:05:13 +0530

    NextGreaterElement updated

--- src/stacksRaghavSir/nextGreaterElement.java
+++ src/stacksRaghavSir/nextGreaterElement.java
@@ -3,30 +3,23 @@ package stacksRaghavSir;
 import java.util.Stack;
 
 public class nextGreaterElement {
-    public static int[] nextGreatest(int[] arr){
-        int n = arr.length;
-        Stack<Integer> st = new Stack<>();
-        int[] res = new int[n];
-        for(int i=n-1;i>=0;i--){
-            if(i==n-1) st.push(arr[i]);
-            if(arr[i]<st.peek()) {
-                res[i] = st.peek();
-                st.push(arr[i]);
-            }
-            else{
-                while(arr[i]>st.peek()){
-                    st.pop();
-                }
-                res[i] = -1;
-            }
-        }
-
-        return res;
-    }
     public static void main(String[] args) {
+        Stack<Integer> st = new Stack<>();
         int[] arr = {1,3,2,1,8,6,3,4};
-        int[] res = nextGreatest(arr);
-        for(int i=0;i<res.length;i++){
+        int n = arr.length;
+        int[] res = new int[n];
+        res[n-1] = -1;
+        st.push(arr[n-1]);
+        for(int i=n-2;i>=0;i--){
+            while(!st.isEmpty() && st.peek()<=arr[i]){
+                st.pop();
+            }
+            if(st.size()==0) res[i] = -1;
+            else res[i] = st.peek();
+
+            st.push(arr[i]);
+        }
+        for(int i=0;i<n;i++){
             System.out.print(res[i]+" ");
         }
     }
