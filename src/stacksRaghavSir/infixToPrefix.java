04023564ab47f9acca28333a4e8be231ea384195
aiharshh <aiharshhublikar@gmail.com>
2025-10-17 18:55:48 +0530

    Infix to Prefix

--- src/stacksRaghavSir/infixToPrefix.java
+++ src/stacksRaghavSir/infixToPrefix.java
@@ -5,6 +5,7 @@ import java.util.Stack;
 public class infixToPrefix {
     public static void main(String[] args) {
         String infix = "9-(5+3)*4/6";
+        System.out.println(infix);
         Stack<String> val = new Stack<>();
         Stack<Character> op = new Stack<>();
         for(int i=0;i<infix.length();i++){
@@ -12,7 +13,7 @@ public class infixToPrefix {
             int ascii = (int)ch;
 //            Ascii Value of '0' -> 48 and '9' -> is 57
             if(ascii>=48 && ascii<=57){
-                String s = " + ch";
+                String s = "" + ch;
                 val.push(s);
             }
             else if(op.size()==0 || ch=='(' || op.peek()=='(') op.push(ch);
@@ -40,12 +41,11 @@ public class infixToPrefix {
                 if(ch=='*' || ch=='/'){
                     if(op.peek()=='*' || op.peek()=='/'){
                         // work
-                        int v2 = val.pop();
-                        int v1 = val.pop();
-                        // + and - aayega he nahi :D
-                        if(op.peek()=='*') val.push(v1*v2);
-                        else if(op.peek()=='/') val.push(v1/v2);
-                        op.pop();
+                        String v2 = val.pop();
+                        String v1 = val.pop();
+                        char o = op.pop();
+                        String t = o + v1 + v2;
+                        val.push(t);
                         // push
                         op.push(ch);
                     }
@@ -54,14 +54,13 @@ public class infixToPrefix {
             }
         }
         while(val.size()>1){
-            int v2 = val.pop();
-            int v1 = val.pop();
-            if(op.peek()=='-') val.push(v1-v2);
-            else if(op.peek()=='+') val.push(v1+v2);
-            else if(op.peek()=='*') val.push(v1*v2);
-            else if(op.peek()=='/') val.push(v1/v2);
-            op.pop();
+            String v2 = val.pop();
+            String v1 = val.pop();
+            char o = op.pop();
+            String t = o + v1 + v2;
+            val.push(t);
         }
-        System.out.print(val.peek());
+        String prefix = val.pop();
+        System.out.print(prefix);
     }
 }
