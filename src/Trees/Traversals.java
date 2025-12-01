cfcee13b17edcce1f596b44a91bdbbe46718ae05
aiharshh <aiharshhublikar@gmail.com>
2025-12-01 22:14:01 +0530

    LevelOrderTraversal

--- src/Trees/Traversals.java
+++ src/Trees/Traversals.java
@@ -1,6 +1,11 @@
 package Trees;
 
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
 public class Traversals {
+    static int n;
     private static void preorder(Node root){
         if(root==null) return;
         System.out.print(root.val + " ");
@@ -19,7 +24,27 @@ public class Traversals {
         postorder(root.right);
         System.out.print(root.val + " ");
     }
+    public static void levelOrder(Node root){
+        Queue<Node> q = new LinkedList<>();
+        if(root!=null) q.add(root);
+        while(q.size()>0){
+            Node front = q.remove();
+            System.out.print(front.val+" ");
+            if(front.left!=null) q.add(front.left); // q.add(front.right);
+            if(front.right!=null) q.add(front.right); // q.add(front.left);
+        }
+        // output - 1 2 3 4 5 6
+//        if we swap left with right and vice versa
+        // we get the output as 1 3 2 7 6 5 4
+    }
+    public static void nthLevel(Node root, int level){
+        if(root==null) return;
+        if(level==n) System.out.print(root.val+" ");
+        nthLevel(root.left, level+1);
+        nthLevel(root.right, level+1);
+    }
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         Node a = new Node(1);
         Node b = new Node(2);
         Node c = new Node(3);
@@ -30,11 +55,20 @@ public class Traversals {
         a.left = b; a.right = c;
         b.left = d; b.right = e;
         c.left = f; c.right = g;
-
+        System.out.println("Preorder traversal : ");
         preorder(a);
         System.out.println();
+        System.out.println("Inorder traversal : ");
         inorder(a);
         System.out.println();
+        System.out.println("Postorder traversal : ");
         postorder(a);
+        System.out.println();
+        System.out.println("Level Order Traversal : ");
+        levelOrder(a);
+        System.out.println();
+        System.out.print("Enter n : ");
+        n = sc.nextInt();
+        nthLevel(a,0);
     }
 }
